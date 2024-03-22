546a808a3c7655ab4c0e538995ba4581984ae5b8
kashtandariya <kashtandariya@gmail.com>
2024-03-22 17:49:25 +0100

    add: esercizio con ciclo for

--- app2.js
+++ app2.js
@@ -8,3 +8,20 @@ let persone = [
   { nome: "Lucia", cognome: "Belli", eta: 15 },
   { nome: "Lorenzo", cognome: "Serafini", eta: 56 }
 ];
+
+//creiamo un array vuoto dove pushiamo il risultato finale
+let risultato = [];
+
+//Ciclo for dove come sempre l'indice deve essere minore alla lunghezza del primo array
+for (let i = 0; i < persone.length; i++) {
+    let persona = persone[i]; //il ciclo for si ripete per ogni persona a tutti gli indici presenti cioè 3 volte
+    let frase;
+    if (persona.eta < 18) {
+        frase = persona.nome + " " + persona.cognome + " non può guidare perché è minorenne.";
+    } else {
+        frase = persona.nome + " " + persona.cognome + " può guidare.";
+    }
+    risultato.push(frase);
+}
+
+console.log(risultato);
