f11bff25f3977d5e750f759d7046aa4eda5fe8d4
kashtandariya <kashtandariya@gmail.com>
2024-03-22 17:43:08 +0100

    add: inizio esercizio 4

---
+++ app2.js
@@ -0,0 +1,10 @@
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+
+//prima creiamo un array con delle persone che sono oggetti con proprietà specifiche
+let persone = [
+  { nome: "Ivan", cognome: "Seghezzi", eta: 21 },
+  { nome: "Lucia", cognome: "Belli", eta: 15 },
+  { nome: "Lorenzo", cognome: "Serafini", eta: 56 }
+];
