9abd97d8639f9973ed318c687bdc125d62d2d4a0
kashtandariya <kashtandariya@gmail.com>
2024-03-22 17:34:12 +0100

    add: sistema stringa arrayCorrente e ordine

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 // Array originale con le stringhe
 let arrayOriginale = ["sole", "STELLE", "Luna"];
 
-// Stampare l'array formattato
-console.log(arrayFormattato);
-
 //definire una funzione
 function formattaStringhe(arrayOriginale) {
   // crea un nuovo array vuoto per memorizzare le stringhe formattate
@@ -17,7 +14,7 @@ function formattaStringhe(arrayOriginale) {
       let arrayCorrente = arrayOriginale[i];//assegno la stringa su cui lavoriamo all'array originale nel ciclo for quindi usiamo l'indice i
 
       // Formatta la stringa: trasforma la prima lettera in maiuscolo e il resto in minuscolo
-      let stringaFormattata = arrayCorrente.charAt(0).toUpperCase() + stringaCorrente.slice(1).toLowerCase();
+      let stringaFormattata = arrayCorrente.charAt(0).toUpperCase() + arrayCorrente.slice(1).toLowerCase();
       //utilizziamo vari metodi, tipo charAt che restituisce il carattere all'indice specificato (0)e poi con toUpperCase lo rendiamo maiuscolo
       //In seguito usiamo il metodo slice all'indice 1 così da partire dal secondo carattere della parola e convertiamo tutti i restanti caratteri in minuscolo
       //il metodo slice serve per restituire una sottostringa della stringa che stiamo usando, potremmo usare anche il metodo .substring che fa una cosa simile da quello che ho capito
@@ -33,3 +30,6 @@ function formattaStringhe(arrayOriginale) {
 // Chiama la funzione per formattare le stringhe
 let arrayFormattato = formattaStringhe(arrayOriginale);
 
+
+// Stampare l'array formattato
+console.log(arrayFormattato);
