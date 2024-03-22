0604d46e68e044a552c635e6d515c154e068493d
kashtandariya <kashtandariya@gmail.com>
2024-03-22 17:19:27 +0100

    add: inizio e fine esercizio senza funzione

---
+++ app.js
@@ -0,0 +1,8 @@
+//A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+// Array originale con le stringhe
+let arrayOriginale = ["sole", "STELLE", "Luna"];
+
+// Stampare l'array formattato
+console.log(arrayFormattato);
+
